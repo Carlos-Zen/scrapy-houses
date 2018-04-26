4a7eacbdc4b8a45328c1688adea400db86982e47
李智超(奕风) <zhchli@uuzu.com>
2018-04-26 10:03:27 +0800

    add danke

--- skywalk/settings.py
+++ skywalk/settings.py
@@ -129,9 +129,9 @@ PROXY_LIST = ['10.6.52.147:3128']
 
 # crawl control
 CRAWL_PAGE = 100
-CLOSESPIDER_ERRORCOUNT = 300
+# CLOSESPIDER_ERRORCOUNT = 300
 # duplate control
-DUPS_LIMIT = 200
+DUPS_LIMIT = 0
 DUPS_STOP = False # 去重中断任务
 DUPS_KEYS = ['city', 'district', 'rent_type', 'address', 'orientation', 'room_area', 'floor', 'building_floor',
              'rental', 'lati', 'longi']
