cb08c2c8b315faaefb9369331bd7416f629ea6b6
李智超(奕风) <zhchli@uuzu.com>
2018-04-28 17:25:07 +0800

    update all key

--- run_spiders.sh
+++ run_spiders.sh
@@ -5,6 +5,6 @@ cd /home/yfeng/skywalk
 pkill -9 scrapy
 nohup scrapy crawl beike >/tmp/beike.log&
 nohup scrapy crawl pinpai58 > /tmp/pinpai.log&
-nohup scrapy crawl baletu > /tmp/balatu.log&
+nohup scrapy crawl baletu > /tmp/baletu.log&
 nohup scrapy crawl ziroom > /tmp/ziroom.log&
 nohup scrapy crawl danke > /tmp/danke.log&
