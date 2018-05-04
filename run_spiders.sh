479885e90da2529a78318c54a0cfb196c83cbbc5
李智超(奕风) <zhchli@uuzu.com>
2018-05-04 16:06:29 +0800

    add response url

--- run_spiders.sh
+++ run_spiders.sh
@@ -3,8 +3,8 @@
 cd /home/yfeng/skywalk
 
 pkill -9 scrapy
-/home/yfeng/local/bin/scrapy crawl beike > /tmp/beike.log
-/home/yfeng/local/bin/scrapy crawl pinpai58 > /tmp/pinpai.log
-/home/yfeng/local/bin/scrapy crawl baletu > /tmp/baletu.log
-/home/yfeng/local/bin/scrapy crawl ziroom > /tmp/ziroom.log
-/home/yfeng/local/bin/scrapy crawl danke > /tmp/danke.log
+/home/yfeng/local/bin/scrapy crawl beike > /tmp/beike.log &
+/home/yfeng/local/bin/scrapy crawl pinpai58 > /tmp/pinpai.log &
+/home/yfeng/local/bin/scrapy crawl baletu > /tmp/baletu.log &
+/home/yfeng/local/bin/scrapy crawl ziroom > /tmp/ziroom.log &
+/home/yfeng/local/bin/scrapy crawl danke > /tmp/danke.log &
